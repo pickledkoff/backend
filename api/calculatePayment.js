d4b7b9585153902b3ae6db90cf10aad27740c7e2
pickledkoff <39276697+pickledkoff@users.noreply.github.com>
2025-03-04 00:26:57 +0200

    Update calculatePayment.js

--- api/calculatePayment.js
+++ api/calculatePayment.js
@@ -1,14 +1,23 @@
-export default function handler(req, res) {
-  // Parse the input data from the request
-  const { apartmentPrice, percentFinancing } = req.body;
+   export default function handler(req, res) {
+     // Log the body of the incoming request
+     console.log('Request Body:', req.body);
 
-  // Create a response object with the message and input data
-  const response = {
-    message: "Great success!",
-    apartmentPrice,
-    percentFinancing
-  };
+     const { apartmentPrice, percentFinancing } = req.body;
 
-  // Send the response as JSON
-  res.status(200).json(response);
-}
+     // Log extracted parameters
+     console.log('Apartment Price:', apartmentPrice);
+     console.log('Percent Financing:', percentFinancing);
+
+     // Create the response object
+     const response = {
+       message: "Great success!",
+       apartmentPrice,
+       percentFinancing
+     };
+
+     // Log the response object
+     console.log('Response:', response);
+
+     // Send the response as JSON
+     res.status(200).json(response);
+   }
