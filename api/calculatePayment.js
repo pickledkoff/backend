f68b3c4ac6e218827d5ddef2e1a6a0bb72e4d37b
pickledkoff <39276697+pickledkoff@users.noreply.github.com>
2025-03-04 10:22:19 +0200

    Update calculatePayment.js

--- api/calculatePayment.js
+++ api/calculatePayment.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
   
-  console.log("Received request body:", req.body); // Debug logging
+ // console.log("Received request body:", req.body); // Debug logging
   
   const { purchasePrice, financePercent, currency } = req.body;
   
